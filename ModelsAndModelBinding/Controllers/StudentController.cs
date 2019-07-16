72b535197e702c0a2f19a6be679dd9ea1e733954
KBonnicksen <kelseybon@gmail.com>
2019-07-15 19:41:40 -0700

    Add project files.

---
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
+
+//Controllers are for routing user input and information
+namespace ModelsAndModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //When data is posted to /Student/Register, this action will execute
+        [HttpPost]
+        public IActionResult Register(IFormCollection data)
+        {
+            //Validate input (lots of work!)
+            //Server side and client side
+            //Add error message on page to notify user
+
+            Student stu = new Student()
+            {
+                FullName = data["full-name"],
+                ProgramOfChoice = data["program"],
+                DateOfBirth = Convert.ToDateTime(data["dob"]),
+                HomeAddress = data["address"]
+            };
+
+            //Add to database
+            return View();
+        }
+
+        //Registration done with Model Binding
+        [HttpGet]
+        public IActionResult RegisterMB()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterMB(Student s)
+        {
+            return View();
+        }
+    }
+}
---
+++ ModelsAndModelBinding/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+
+namespace ModelsAndModelBinding.Models
+{
+    /// <summary>
+    /// Represents an indicidual student
+    /// </summary>
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        /// <summary>
+        /// Legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Program the student intends to complete
+        /// </summary>
+        public string ProgramOfChoice { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+        public string HomeAddress { get; set; }
+    }
+}
